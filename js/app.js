ae4bf39fa08d662c9fb98482a076634a532e9a05
batogov <thesiamesecat19@gmail.com>
2017-05-09 14:58:24 +0300

    Добавлен попап + новые стили

--- js/app.js
+++ js/app.js
@@ -79,22 +79,179 @@ var Post = React.createClass({
 });
 
 
+var NewPostPopup = React.createClass({
+    getInitialState: function() {
+        return {
+            popupIsHidden: true,
+            agreeNotChecked: true,
+            authorIsEmpty: true,
+            titleIsEmpty: true,
+            textIsEmpty: true
+        };
+    },
+
+    componentDidMount: function() {
+        var self = this;
+        window.ee.addListener('OpenPopupBtnClick', function() {
+            self.openPopup();
+        });
+    },
+
+    onFieldChange: function(fieldName, e) {
+        if (e.target.value.trim().length > 0) {
+            this.setState({[fieldName]: false})
+        } else {
+            this.setState({[fieldName]: true})
+        }
+    },
+
+    onBtnClickHandler: function(e) {
+        e.preventDefault();
+
+        var authorElem = ReactDOM.findDOMNode(this.refs.author);
+        var titleElem = ReactDOM.findDOMNode(this.refs.title);
+        var textElem = ReactDOM.findDOMNode(this.refs.text);
+        var checkElem = ReactDOM.findDOMNode(this.refs.check);
+
+        // Изображение заметки генерируем случайно
+        var img = Math.floor(Math.random() * 9) + 1;
+
+        var item = [{
+            img: img,
+            author: authorElem.value,
+            text: textElem.value,
+            title: titleElem.value
+        }];
+
+        window.ee.emit('AddPost', item);
+
+        // Очищаем форму после добавления заметки
+        authorElem.value = "";
+        textElem.value = "";
+        titleElem.value = "";
+        checkElem.checked = false;
+
+        this.setState({
+            popupIsHidden: true,
+            authorIsEmpty: true,
+            titleIsEmpty: true,
+            textIsEmpty: true,
+            agreeNotChecked: true
+        });
+    },
+
+    closePopup: function() {
+        this.setState({popupIsHidden: true});
+    },
+
+    openPopup: function() {
+        this.setState({popupIsHidden: false});
+    },
+
+    onCheckRuleClick: function(e) {
+        this.setState({agreeNotChecked: !this.state.agreeNotChecked});
+    },
+
+    render: function() {
+        var popupIsHidden = this.state.popupIsHidden,
+            agreeNotChecked = this.state.agreeNotChecked,
+            authorIsEmpty = this.state.authorIsEmpty,
+            titleIsEmpty = this.state.titleIsEmpty,
+            textIsEmpty = this.state.textIsEmpty;
+
+        return (
+            <div className={popupIsHidden ? "popup popup--hidden" : "popup"}>
+                <div className="popup-block">
+                    <form>
+                        <input
+                            type="text"
+                            defaultValue=""
+                            placeholder="Имя автора"
+                            onChange={this.onFieldChange.bind(this, 'authorIsEmpty')}
+                            ref="author"
+                        />
+
+                        <input
+                            type='text'
+                            defaultValue=''
+                            placeholder='Название заметки'
+                            onChange={this.onFieldChange.bind(this, 'titleIsEmpty')}
+                            ref='title'
+                        />
+
+                        <textarea
+                            defaultValue=''
+                            placeholder='Текст заметки'
+                            rows="5"
+                            onChange={this.onFieldChange.bind(this, 'textIsEmpty')}
+                            ref='text'>
+                        </textarea>
+
+                        <div className="popup-block__submit-group">
+                            <label>
+                                <input
+                                    type='checkbox'
+                                    defaultChecked={false}
+                                    onChange={this.onCheckRuleClick}
+                                    ref='check' />
+                                Я согласен с правилами
+                            </label>
+
+                            <button
+                                onClick={this.onBtnClickHandler}
+                                ref='button'
+                                disabled={agreeNotChecked || authorIsEmpty || titleIsEmpty || textIsEmpty}>
+                                Добавить заметку
+                            </button>
+                        </div>
+                    </form>
+                </div>
+                <div className="popup-back" onClick={this.closePopup}></div>
+            </div>
+        );
+    }
+});
+
+
 var App = React.createClass({
     getInitialState: function() {
         return {
-            posts: posts
+            posts: posts,
+            popupIsHidden: true
         };
+    },
+
+    componentDidMount: function() {
+        var self = this;
+
+        window.ee.addListener('AddPost', function(item) {
+            // Cоздаём новый массив, в котором первым элементом ставим
+            // новую заметку, чтобы она была верхней в списке
+            var nextPost = item.concat(self.state.posts);
+            self.setState({posts: nextPost});
+        });
+    },
+
+    componentWillUnmount: function() {
+        window.ee.removeListener('AddPost');
     },
 
     render: function() {
         return (
             <div className="container">
-                <h1>Все заметки</h1>
+                <div className="header">
+                    <h1>Все заметки</h1>
+                    <a className="header__btn" href="#" onClick={this.openPopup}>Добавить новую</a>
+                </div>
                 <Posts data={this.state.posts} />
+                <NewPostPopup />
             </div>
         );
-    }
+    },
 
+    openPopup: function() {
+        window.ee.emit('OpenPopupBtnClick');
+    }
 });
 
 
